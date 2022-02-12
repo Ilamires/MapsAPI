a147b50885549f11ca39e3872aa1eaa73d2fca97
morning <v.shamraev1@yandex.ru>
2022-02-12 20:15:13 +0300

    Map views

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QButtonGroup
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from log import Map
@@ -11,12 +11,18 @@ class MainWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.setWindowTitle('Map')
+        self.image.setFocus()
         self.delta = 0.005
         self.map = Map()
         self.btn_s.clicked.connect(self.search)
         self.mapRadio.setChecked(True)
         self.view = 'map'
         self.map.toponim('Москва')
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.mapRadio)
+        self.button_group.addButton(self.satRadio)
+        self.button_group.addButton(self.sklRadio)
+        self.button_group.buttonClicked.connect(self.ViewChange)
         name = self.map.maping(self.delta, self.view)
         pixmap = QPixmap(name)
         self.image.setPixmap(pixmap)
@@ -43,16 +49,19 @@ class MainWidget(QMainWindow):
         pixmap = QPixmap(name)
         self.image.setPixmap(pixmap)
 
+    def ViewChange(self, button):
+        if button.text() == 'Схема':
+            self.view = 'map'
+        elif button.text() == 'Спутник':
+            self.view = 'sat'
+        elif button.text() == 'Гибрид':
+            self.view = 'skl'
+        self.rendering()
+
     def search(self):
         text = self.lineEdit.text()
         if text != "":
             self.map.toponim(text)
-            if self.mapRadio.isChecked():
-                self.view = 'map'
-            elif self.satRadio.isChecked():
-                self.view = 'sat'
-            elif self.sklRadio.isChecked():
-                self.view = 'skl'
             self.rendering()
             self.image.setFocus()
 
